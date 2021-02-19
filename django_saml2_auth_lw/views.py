3d558e0d0e3b31a61b70ec9fc145578dc0d12c8a
Frinze <frinzekt@gmail.com>
2021-02-19 13:19:33 +0800

    initial linting

--- django_saml2_auth_lw/views.py
+++ django_saml2_auth_lw/views.py
@@ -141,14 +141,18 @@ def _create_new_user(username, email, firstname, lastname):
     user = User.objects.create_user(username, email)
     user.first_name = firstname
     user.last_name = lastname
-    groups = [Group.objects.get(name=x) for x in settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('USER_GROUPS', [])]
+    groups = [Group.objects.get(name=x) for x in settings.SAML2_AUTH.get(
+        'NEW_USER_PROFILE', {}).get('USER_GROUPS', [])]
     if parse_version(get_version()) >= parse_version('2.0'):
         user.groups.set(groups)
     else:
         user.groups = groups
-    user.is_active = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('ACTIVE_STATUS', True)
-    user.is_staff = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('STAFF_STATUS', True)
-    user.is_superuser = settings.SAML2_AUTH.get('NEW_USER_PROFILE', {}).get('SUPERUSER_STATUS', False)
+    user.is_active = settings.SAML2_AUTH.get(
+        'NEW_USER_PROFILE', {}).get('ACTIVE_STATUS', True)
+    user.is_staff = settings.SAML2_AUTH.get(
+        'NEW_USER_PROFILE', {}).get('STAFF_STATUS', True)
+    user.is_superuser = settings.SAML2_AUTH.get(
+        'NEW_USER_PROFILE', {}).get('SUPERUSER_STATUS', False)
     user.save()
     return user
 
@@ -171,10 +175,14 @@ def acs(r):
     if user_identity is None:
         return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
 
-    user_email = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('email', 'Email')][0]
-    user_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('username', 'UserName')][0]
-    user_first_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('first_name', 'FirstName')][0]
-    user_last_name = user_identity[settings.SAML2_AUTH.get('ATTRIBUTES_MAP', {}).get('last_name', 'LastName')][0]
+    user_email = user_identity[settings.SAML2_AUTH.get(
+        'ATTRIBUTES_MAP', {}).get('email', 'Email')][0]
+    user_name = user_identity[settings.SAML2_AUTH.get(
+        'ATTRIBUTES_MAP', {}).get('username', 'UserName')][0]
+    user_first_name = user_identity[settings.SAML2_AUTH.get(
+        'ATTRIBUTES_MAP', {}).get('first_name', 'FirstName')][0]
+    user_last_name = user_identity[settings.SAML2_AUTH.get(
+        'ATTRIBUTES_MAP', {}).get('last_name', 'LastName')][0]
 
     target_user = None
     is_new_user = False
@@ -182,13 +190,17 @@ def acs(r):
     try:
         target_user = User.objects.get(username=user_name)
         if settings.SAML2_AUTH.get('TRIGGER', {}).get('BEFORE_LOGIN', None):
-            import_string(settings.SAML2_AUTH['TRIGGER']['BEFORE_LOGIN'])(user_identity)
+            import_string(settings.SAML2_AUTH['TRIGGER']['BEFORE_LOGIN'])(
+                user_identity)
     except User.DoesNotExist:
-        new_user_should_be_created = settings.SAML2_AUTH.get('CREATE_USER', True)
-        if new_user_should_be_created: 
-            target_user = _create_new_user(user_name, user_email, user_first_name, user_last_name)
+        new_user_should_be_created = settings.SAML2_AUTH.get(
+            'CREATE_USER', True)
+        if new_user_should_be_created:
+            target_user = _create_new_user(
+                user_name, user_email, user_first_name, user_last_name)
             if settings.SAML2_AUTH.get('TRIGGER', {}).get('CREATE_USER', None):
-                import_string(settings.SAML2_AUTH['TRIGGER']['CREATE_USER'])(user_identity)
+                import_string(settings.SAML2_AUTH['TRIGGER']['CREATE_USER'])(
+                    user_identity)
             is_new_user = True
         else:
             return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
@@ -231,7 +243,8 @@ def signin(r):
 
     try:
         if 'next=' in unquote(next_url):
-            next_url = _urlparse.parse_qs(_urlparse.urlparse(unquote(next_url)).query)['next'][0]
+            next_url = _urlparse.parse_qs(
+                _urlparse.urlparse(unquote(next_url)).query)['next'][0]
     except:
         next_url = r.GET.get('next', _default_next_url())
 
