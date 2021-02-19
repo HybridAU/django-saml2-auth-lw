4c81c6f475c8a4567b237b6a4526d605f276f78e
Frinze <frinzekt@gmail.com>
2021-02-19 14:01:20 +0800

    remove versioning in the setup

--- setup.py
+++ setup.py
@@ -10,14 +10,11 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='django_saml2_auth_lw',
-
-    version='2.2.1',
-
     description='Django SAML2 Authentication Made Easy. Easily integrate with SAML2 SSO identity providers like Okta',
     long_description=long_description,
 
